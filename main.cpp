2f27588f67ff146f4c346246b8b694af64cfec31
SamanthaGuanipa <70000311+SamanthaGuanipa@users.noreply.github.com>
2020-12-01 08:00:11 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-
+#include <string> 
+#include <sstream>
+#include "Usuario.h"
+#include "Organizacion.h"
+#include "Funciones.h"
+using namespace std;
 int main() {
-  std::cout << "Hello World!\n";
+  Organizacion org;
+  Coordinador coord;
+  Donador don;
+  Funciones Fun;
+  org.getPagos();
+  don.agregarPago(&org);
+  Fun.agregarPago();
+  Fun.consultarHistorial();
 }
