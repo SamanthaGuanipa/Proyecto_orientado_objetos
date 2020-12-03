22ec1757e5ae1541fab0b81d26983c79757fc6ca
SamanthaGuanipa <70000311+SamanthaGuanipa@users.noreply.github.com>
2020-12-03 15:41:58 -0600

    Update Organizacion.h

--- Organizacion.h
+++ Organizacion.h
@@ -12,7 +12,6 @@
  *
  */
 
- //USAR EJEMPLO AUTO Y LLANTAS PARA LA COMPOSICIÓN
 
 #ifndef ORGANIZACION_H_
 #define ORGANIZACION_H_
@@ -29,27 +28,32 @@ class Organizacion{
 
  protected:
  
- //Declaro los atributos 
-  
-  string nombre;
+ //Se declaran los atributos 
   int posicion = 0;
+  string nombre;
+  string user;
+  string concept;
+  float cantidad;
+  string name[100];
+  string usuario[100];
   string concepto[100];
   float pago[100];
-  Coordinador coordinadores[10];
   int coordPosicion = 0;
-  Donador donadores[100]; 
   int donaPosicion=0; 
  
   
 public:
+  
   //Constructor por deffault
   
   Organizacion();
 
+  
   //Constructor que tiene variables preestablecidas
   
-  Organizacion(string nom);
+  Organizacion(string nom, string user, string conc, float cant );
  
+
   void set_nombre(string);
   string get_nombre();
   void getPagos();
@@ -61,47 +65,76 @@ public:
 //Constructor por deffault 
 Organizacion::Organizacion(){
   nombre=""; 
+  user="";
+  concept="";
+  cantidad=0.0;
 }
-Organizacion::Organizacion(string nom){
-  nombre=nom; 
+
+
+Organizacion::Organizacion(string nom, string user_1, string conce, float cant){
+  nombre=nom;
+  user=user_1;
+  concept=conce; 
+  cantidad=cant;
 }
+
 //Se establecen los getters de la clase Organizacion
 
 string Organizacion::get_nombre(){
   return nombre;
 }
+
 //Se establecen los setters de la clase Organizacion
 
 void Organizacion::set_nombre(string nomb){
   nombre=nomb;
 }
 
+//Se crea el método que recibe los pagos a través de arreglos
+
 void Organizacion::getPagos(){
+
   int numeroPagos;
+
   cout<<"Ingrese el número de pagos a realizar"<<endl;
+  
+  //Se recibe el numero de pagos a realizar 
   cin>>numeroPagos;
+  
+  //Se inicializan las variables
   int minimo = posicion;
+
+  //Inicia el ciclo for 
   for(int i=posicion; i<minimo+numeroPagos; i++)
   {
-    cout<<"Ingrese el pago a realizar "<<(i+1)<<": ";
-    cin>>pago[i] ;
+
+    cout<<"Ingrese su nombre "<<(i+1)<<": ";
+    cin>>name[i] ;
+    cout<<"Ingrese su usuario "<<(i+1)<<": ";
+    cin>>usuario[i];
     cout<<"Ingrese el concepto del pago a realizar "<<(i+1)<<": ";
     cin>>concepto[i] ;
+    cout<<"Ingrese el pago a realizar "<<(i+1)<<": ";
+    cin>>pago[i] ;
+    cout<<"Su pago fue realizado con éxito."<<endl;
     posicion++;
   }
 
 }
 
+//Se crea el método que enseña los pagos realizados
 void Organizacion::historial(){
  for (int i=0; i<posicion; i++)
  {
    cout<<"Pago "<<i+1<<":\n";
+   cout<<"Nombre: "<<name[i]<<endl;
+   cout<<"Usuario: "<<usuario[i]<<endl; 
    cout<<"Concepto: "<<concepto[i]<<endl;
    cout<<"Cantidad: "<<pago[i]<<endl;
  }
 }
 
-
+//Se crea la función que suma el total de pagos realizados
 float Organizacion::calcularTotal(){
   int suma=0;
   for (int i=0; i<posicion;i++)
@@ -114,4 +147,4 @@ float Organizacion::calcularTotal(){
 
 
 
-#endif
+#endif //ORGANIZACION_H_
