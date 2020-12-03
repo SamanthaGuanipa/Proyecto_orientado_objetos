bde2a449a3736016b369a0a5bc8064e55a724be1
SamanthaGuanipa <70000311+SamanthaGuanipa@users.noreply.github.com>
2020-12-03 15:41:15 -0600

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -1,3 +1,15 @@
+/*
+ * Proyecto POO
+ * Samantha Daniela Guanipa Ugas
+ * A01703936
+ * 26/11/2020
+ */
+
+/*
+ * Clase Funciones fue creada para mandar a llamar las funciones que dependen entre Usuario y Organizacion. Se llama funciones y recibe a las clases "Organizacion.h" y "Usuario.h"
+ * 
+ *
+ */
 #ifndef FUNCIONES_H_
 #define FUNCIONES_H_
 
@@ -9,20 +21,37 @@
 #include "Organizacion.h"
 
 using namespace std; 
+
+
+//Declaracion de la clase Funciones 
 class Funciones{
+
+  
+  //Se declaran los atributos 
   protected:
+  string nombrecoord,nombredona, usuariocoord, usuariodona, concepto;
+  int cantidad;
   Organizacion org;
-  Coordinador coord;
-  Donador don;
+  Coordinador coordinadores[10];
+  Donador donadores[100];
   Donador* donador;
+  int donaPosicion=0;
+  int coordPosicion=0;
+ 
+ 
+ //Se declaran los métodos 
   public:
   void agregarPago(Organizacion *org);
-  void consultarHistorial();
+  void consultarHistorial(Organizacion org);
   void agregarCoordinador();
   void agregarDonador();
+  void mostrarCoordinador();
+  void mostrarDonador();
 
   
 };
+
+//Se crea la función agregarPago que recibe objeto Organizacion
 
 void Funciones::agregarPago(Organizacion *org){
 
@@ -30,30 +59,66 @@ void Funciones::agregarPago(Organizacion *org){
   
 }
 
-void Funciones::consultarHistorial(){
+//Se crea la función consultarHistorial que recibe objeto Organizacion
+
+void Funciones::consultarHistorial(Organizacion org){
  org.historial();
 
 }
+
+//Se crea la función agregarCoordinador que recibe objeto Coordinador
 void Funciones::agregarCoordinador(){
-  string nombre, usuario;
   cout<<"Nombre: ";
-  cin>>nombre;
+  cin>>nombrecoord;
   cout<<"Usuario: ";
-  cin>>usuario;
-  coordinadores[coordPosicion] = Coordinador(nombre,usuario);
+  cin>>usuariocoord;
+  coordinadores[coordPosicion] = Coordinador(nombrecoord,usuariocoord);
   coordPosicion++;
 
 }
+
+//Se crea la funcion agregarDonador que recibe objeto Donador
 void Funciones::agregarDonador(){
-  string nombre, usuario;
+  
   cout<<"Nombre: ";
-  cin>>nombre;
+  cin>>nombredona;
   cout<<"Usuario: ";
-  cin>>usuario;
-  donadores[donaPosicion] = Donador(nombre,usuario);
-  coordPosicion++;
+  cin>>usuariodona;
+  cout<<"Cantidad: ";
+  cin>>cantidad;
+  cout<<"Concepto: ";
+  cin>>concepto;
+  donadores[donaPosicion] = Donador(nombredona,usuariodona,cantidad,concepto);
+  donaPosicion++;
+}
+
+//Se crea la funcion mostrarCoordinador que enseña los arreglos creados a través de un ciclo for 
+
+void Funciones::mostrarCoordinador(){
+
+ for (int i=0; i<coordPosicion; i++)
+ {
+   cout<<"Coordinador "<<i+1<<":\n";
+   cout<<"Nombre: "<<nombrecoord<<endl;
+   cout<<"Usuario: "<<usuariocoord<<endl;
+ }
+}
+
+//Se crea la funcion mostrarDonador que enseña los arreglos creados a través de un ciclo for 
+
+void Funciones::mostrarDonador(){
+ for (int i=0; i<=donaPosicion; i++)
+ {
+   cout<<"Donador"<<i+1<<":\n";
+   cout<<"Nombre: "<<nombredona<<endl;
+   cout<<"Usuario: "<<usuariodona<<endl;
+   cout<<"Cantidad: "<<cantidad<<endl;
+   cout<<"Concepto: "<<concepto<<endl;
+ }
 }
 
 
-#endif
+
+
+#endif //FUNCIONES_H_
 
